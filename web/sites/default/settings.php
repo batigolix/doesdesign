dbdbe1f4f9ead1c3e022e6c21ea6c7129c59af9a
Boris Doesborg <bdoesborg@gmail.com>
2021-02-05 15:01:29 +0100

    wip

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -88,7 +88,18 @@
  * ];
  * @endcode
  */
-$databases = [];
+
+$databases['default']['default'] = [
+  'database' => 'drupal8',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => '172.18.0.1',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+  'unix_socket' => '/var/lib/mysql/mysql.sock',
+];
 
 /**
  * Customizing database settings.
