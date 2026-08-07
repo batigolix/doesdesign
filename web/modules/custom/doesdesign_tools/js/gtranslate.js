8af3fbbfe1d439eaa5d8fdcbbdd787704555e135
Boris Doesborg <boris.doesborg@finalist.nl>
2026-08-07 20:33:08 +0200

    Remove 4 D8 legacy block plugins from doesdesign_tools + templates/libs/images cleanup (doesdesign-ezto/05vr/npkb/t6sy)

--- web/modules/custom/doesdesign_tools/js/gtranslate.js
+++
@@ -1,8 +0,0 @@
-// AI generated
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'nl',
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-    autoDisplay: false
-  }, 'google_translate_element');
-}
