735323af8e210b907a658faa5b1d30f22847c968
Boris Doesborg <boris.doesborg@finalist.nl>
2026-08-07 09:34:19 +0200

    Remove unused AccountProxyInterface use statement (doesdesign-15ii)

--- web/modules/custom/doesdesign_tools/src/Form/ContactForm.php
+++ web/modules/custom/doesdesign_tools/src/Form/ContactForm.php
@@ -4,7 +4,6 @@ namespace Drupal\doesdesign_tools\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Session\AccountProxyInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
